fe38233088f032a5f959c623ce5a81dd935c84b6
chaow <chaowang970413@gmail.com>
2022-07-14 05:29:14 -0700

    Express REST API with MongoDB and Mongoose Part 2

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -14,7 +14,7 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = 'mongodb://127.0.0.1:27017/conFusion';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
