a54a42eea21c056b7d438af455a7948347776b5f
chaow <chaowang970413@gmail.com>
2022-07-17 19:23:43 -0700

    Assignment 4

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -1,30 +1,34 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
+var index = require('./routes/index');
+var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter')
+var config = require('./config');
 
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+const Dishes = require('./models/dishes');
 
 var passport = require('passport');
 var authenticate = require('./authenticate');
 
-var config = require('./config');
-
-const mongoose = require('mongoose');
-
-const Dishes = require('./models/dishes');
-
 const url = config.mongoUrl;
-const uploadRouter = require('./routes/uploadRouter');
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url, {
+    useMongoClient: true,
+    
+  });
 
 connect.then((db) => {
     console.log("Connected correctly to server");
@@ -32,7 +36,6 @@ connect.then((db) => {
 
 var app = express();
 
-// Secure traffic only
 app.all('*', (req, res, next) => {
   if (req.secure) {
     return next();
@@ -42,47 +45,35 @@ app.all('*', (req, res, next) => {
   }
 });
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+
+
+app.use('/', index);
+app.use('/users', users);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use('/favorites',favoriteRouter);
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function(err, req, res, next) { 
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
